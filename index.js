c84d43f6e3b6f159fa76d03337337f6da047bd4f
rjeantet <rose.jeantet@gmail.com>
2023-10-20 15:31:37 +0200

    3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.use(express.json());
+app.use(morgan('tiny'));
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 let persons = [
   {
