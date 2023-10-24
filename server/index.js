8c464b687d6f73dcea06ae12db7cdd936d0e5a9d
rjeantet <rose.jeantet@gmail.com>
2023-10-24 10:29:17 +0200

    Preparing for deployment on render

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(morgan('tiny'));
 
@@ -128,7 +129,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
