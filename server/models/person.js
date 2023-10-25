b9eb5ef7519ad8b0a3a48ea6104dddbee03f02c1
rjeantet <rose.jeantet@gmail.com>
2023-10-25 12:41:12 +0200

    3.20*: Phonebook database, step8

--- server/models/person.js
+++ server/models/person.js
@@ -15,8 +15,20 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, minLength: 3, required: true },
-  number: { type: String, required: true },
+  name: { type: String, minLength: 3, required: [true, 'name required'] },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d+/.test(v);
+        console.log('v', v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! It should be in the format XX-XXXX... or XXX-XXXX...`,
+    },
+    required: [true, 'phone number required'],
+  },
 });
 
 personSchema.set('toJSON', {
