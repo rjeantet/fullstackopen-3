9bd4dd76b00048d0a1bf2d0b9822a681e5e075ef
rjeantet <rose.jeantet@gmail.com>
2023-10-25 08:11:08 +0200

    uibuild

--- server/index.js
+++ server/index.js
@@ -3,16 +3,39 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 
+//-------------- Models -----------------//
 const Person = require('./models/person');
 
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(morgan('tiny'));
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+//-------------- Middleware --------------//
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
 app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(morgan('tiny'));
+app.use(express.static('dist'));
 
+morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(
     ':method :url :status :res[content-length] - :response-time ms :body',
@@ -22,6 +45,7 @@ app.use(
   )
 );
 
+//-------------- Routes -----------------//
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
@@ -47,17 +71,22 @@ app.get('/api/people', (request, response) => {
 
 //[x]: tested ok
 app.get('/api/people/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(500).end();
+    });
 });
 
 //[x]: tested ok
-app.put('/api/people/:id', (request, response) => {
+app.put('/api/people/:id', (request, response, next) => {
   const body = request.body;
   const person = {
     name: body.name,
@@ -97,20 +126,16 @@ app.post('/api/people', (request, response) => {
   });
 });
 
-//[x]: tested ok
-app.delete('/api/people/:id', (request, response) => {
-  const body = request.body;
-  Person.findByIdAndDelete(request.params.id, body)
+//[]:
+app.delete('/api/people/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
     .then((person) => {
-      response.json(person);
+      response.status(204).end();
     })
     .catch((error) => next(error));
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-
+app.use(errorHandler);
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
